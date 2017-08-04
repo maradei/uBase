675585ff9ba127a7602333b71f25a2e45bdd0d4a
levy <levyssantiago@gmail.com>
2017-08-04 17:20:16 -0300

    Bibliotecas utilizadas na função remover, podem ser usadas por outras

---
+++ src/bibliotecaRemover.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bibliotecaStrings.h"
+
+int removerTabela(char *nomeTabela){
+	char *caminho = getCaminhoTabela(nomeTabela);
+	if(caminho == NULL){
+		return 0;
+	}
+	else if(arquivoExiste(caminho)){
+		return !remove(caminho);
+	}else{
+		return -1;
+	}
+}
+
+int arquivoExiste(char *caminho){
+	FILE *fil = NULL;
+
+	fil = fopen(caminho,"rb");
+	if(fil != NULL){ /*Se existe um arquivo com esse caminho*/
+		fclose(fil);
+		return 1;
+	}else{ /*Não existe um arquivo com esse caminho*/
+		return 0;
+	}
+}
